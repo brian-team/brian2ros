9e2e14bf87a37df47208680e2ece51bd7e88ee13
macauwet <cauwet@isir.upmc.fr>
2025-06-17 17:53:56 +0200

    feat: Ajuster les paramètres des neurones et ajouter une fonction de changement de phase

--- src/ros_standalone/examples/microphone/update_jeffres.py
+++ src/ros_standalone/examples/microphone/update_jeffres.py
@@ -16,16 +16,16 @@ tau = 0.2*ms
 
 # Thresholds
 a = 1
-p = 3
+p = 1.5
 tau_thresh = 5*ms
-
+lam = 0.5
 # Sin wave input parameters
 amplitude = 32  # Amplitude for 16-bit audio
-phase_shift = 0.5 * np.pi  # Phase shift for the sound
-chrono = 100
+phase_shift = 1 * np.pi  # Phase shift for the sound
+chrono = 50
 max_delay = 0.2/343 * second
 eqs_ears = '''
-dx/dt = (input - x)/tau_ear : 1
+dx/dt = (input - x)/tau_ear : 1 (unless refractory)
 input = 3*clip(sound, 0, inf)**(1/3) : 1
 sound = sound_left * (1 - i) + sound_right * i : 1
 sound_left = amplitude * sin(2 * pi * frequency * (t + left_delay)) : 1
@@ -36,20 +36,26 @@ left_delay = -0.5 * max_delay * sin(phase_shift) : second
 
 dthresh/dt = (a * clip(x, 0, inf) - thresh) / tau_thresh : 1
 frequency : Hz
+rec_freq : Hz
+phase_shift : 1
 '''
 reset = '''
+thresh = p * thresh + lam * x
 x = 0
-thresh = p * thresh
 '''
 ears = NeuronGroup(2, eqs_ears, threshold='x>thresh and x>0.05',reset=reset,
-                    name='ears', method='euler')
+                    name='ears', method='euler', refractory=0.5*ms)
 ears.thresh = [1, 1]
-ears.frequency = 440*Hz  # Initial frequency for the sound input  
+ears.frequency = 720*Hz  # Initial frequency for the sound input  
+ears.rec_freq = ears.frequency  # Record the frequency for monitoring
+ears.phase_shift = phase_shift  # Initial phase shift for the sound input
 # @network_operation(dt=chrono*ms)
 # def change_freq():
-#     ears.frequency += 0 * Hz # Increment frequency for demonstration
+#     ears.frequency *= 1.2 # Increment frequency for demonstration
 
-
+@network_operation(dt=chrono*10*ms)
+def change_phase():
+    ears.phase_shift += 0.1 * np.pi  # Increment phase shift for demonstration
 eqs_neurons = '''
 dv/dt = -v / tau : 1
 '''
@@ -65,12 +71,20 @@ synapses.delay['i==1'] = '(1.0*(num_neurons-j-1))/(num_neurons-1)*1.1*max_delay'
 wta = Synapses(neurons, neurons, on_pre='v -= 0.65')  
 wta.connect(condition='i != j') 
 
+eqs_radar = '''
+dv/dt = 
+'''
+
+radar = NeuronGroup(1, eqs_radar)
+radar_synapses = Synapses(ears, radar, on_pre=)
+radar_synapses.connect()
 
 res = SpikeMonitor(ears)
 spikes = SpikeMonitor(neurons)
 x_ears = StateMonitor(ears, 'x', record=True)
+state_rad = StateMonitor(radar, 'v', record=True)
 th_ears = StateMonitor(ears, 'thresh', record=True)
-run(1* second, report='text')
+run(5* second, report='text')
 
 # Plotting the results
 plt.figure(figsize=(18, 16))
@@ -101,12 +115,10 @@ plt.ylabel('Threshold value')
 plt.legend()
 
 plt.subplot(4, 1, 4)
-plt.plot(spikes.t / ms, spikes.i, '.k')
+plt.plot(state_rad.t / ms, state_rad.v[0])
 plt.title('Spike Times of Neurons')
 plt.xlabel('Time (ms)')
 plt.ylabel('Neuron Index')
-plt.xlim(0, 1000)
-plt.ylim(-1, num_neurons)
 
 plt.tight_layout()
 plt.show()
