562a02ede481439cc29c7161b25ef2f519fa1187
macauwet <cauwet@isir.upmc.fr>
2025-07-23 14:28:16 +0200

    feat: Réorganiser les paramètres des oreilles, ajouter des sections pour les neurones, le radar et la direction dans jeffres_brian.py

--- src/ros_standalone/examples/microphone/jeffres_brian.py
+++ src/ros_standalone/examples/microphone/jeffres_brian.py
@@ -6,16 +6,15 @@ defaultclock.dt = 1 / 48_000 * second
 # Sound
 audio = TimedArray(10 * randn(50000), dt=defaultclock.dt) # white noise
 
-# Ears parameters
+# EARS PARAMETERS
+
 sound_speed = 343.0*metre/second
 distance_between_ears = 0.22*metre
 sigma_ear = .1
 tau_ear = 0.5*ms
 max_delay = distance_between_ears / sound_speed
 lam = 0.7  
-# Coincidence detectors
-num_neurons = 30
-tau = 0.2*ms
+angular_speed = 2 * pi / second
 
 # Thresholds
 a = 1
@@ -23,17 +22,32 @@ p = 1.5
 tau_thresh = 5*ms
 
 # Filter parameters
-
 f_min = 100 * Hz
 f_max = 2500 * Hz  
 nb_band = 16
-
 BW = 0.2
 fs = 48000 * Hz 
 
-#
-angular_speed = 2 * pi / second
-# Ears and sound motion around the head (constant angular speed)
+# NEURONS PARAMETERS
+num_neurons = 30
+tau = 0.2*ms
+
+# RADAR PARAMETERS
+tau_radar = 25 * ms
+num_radar = 15
+
+# DIRECTION PARAMETERS
+num_direction = 2 # Left and Right
+tau_direction = 2 * ms  
+tau_target = 20 * ms
+max_vel = 1.82 
+alpha_vel = 2 # acceleration factor
+a_front = 0.2 # Determines the front
+min_front = num_radar//2 - np.floor(num_radar*a_front/2)
+max_front = (num_radar//2 + np.floor(num_radar*a_front/2)) + 1
+
+# EARS
+
 eqs_ears = '''
 f_center : Hz (constant)
 xn = audio(t -delay) : 1 
@@ -82,8 +96,9 @@ ears.a2 = '(1 - alpha) / a0'  # Coefficient for output two steps back
 
 ears.run_regularly('xn2 = xn1 ; xn1 = xn ; yn2 = yn1 ; yn1 = yn', dt=defaultclock.dt, when='end')
 
-
 ears.thresh = np.ones(2 * nb_band) * 5000  # Initialize thresholds
+
+# NEURONS
 
 eqs_neurons = '''
 dv/dt = -v / tau : 1
@@ -96,12 +111,8 @@ synapses.connect('i % nb_band == j // num_neurons')
 
 synapses.delay['i//nb_band==0'] = '(1.0*j)/(num_neurons-1)*1.1*max_delay'
 synapses.delay['i//nb_band==1'] = '(1.0*(num_neurons-j-1))/(num_neurons-1)*1.1*max_delay'
-
-#wta = Synapses(neurons, neurons, on_pre='v -= 0.65')  
-#wta.connect(condition='i != j') 
-
-tau_radar = 25 * ms  # Time constant for the radar
-num_radar = 15
+ 
+# RADAR
 
 eqs_radar = '''
 dv/dt = -v / tau_radar: 1
@@ -111,6 +122,8 @@ radar = NeuronGroup(num_radar * nb_band, eqs_radar, threshold='v>1', method='eul
 radar_synapses = Synapses(neurons, radar, on_pre='v += 0.65')
 radar_synapses.connect(condition='j == i//(num_neurons//num_radar)')
 
+# COMBINATION
+
 eqs_combi = '''
 dv/dt = -v / tau_radar : 1
 '''
@@ -118,24 +131,19 @@ combination = NeuronGroup(num_radar, eqs_combi, threshold='v>1', reset='v=0', me
 combination_synapses = Synapses(radar, combination, on_pre='v += 0.65')
 combination_synapses.connect('j == i // nb_band') 
 
-num_direction = 2 # Left and Right
-tau_direction = 2 * ms  
-tau_target = 20 * ms
-max_vel = 1.82 
+# DIRECTION DETECTION
+
 eqs_direction = '''
 dv/dt = clip(-v, -max_vel, max_vel) / tau_direction : 1
 dvel/dt = (v - vel) / tau_target : 1
 '''
 
-alpha_vel = 2 # acceleration factor
-a_front = 0.2 # Determines the front
-min_front = num_radar//2 - np.floor(num_radar*a_front/2)
-max_front = (num_radar//2 + np.floor(num_radar*a_front/2)) + 1
-
 direction = NeuronGroup(num_direction, eqs_direction, method='euler', name='direction', dt=0.1*ms)
 direction_synapses = Synapses(combination, direction, on_pre='v += alpha_vel * ((abs(i-((num_radar-1)/2))/((num_radar-1)/2))-a_front)')
 direction_synapses.connect(i=np.arange(0,max_front,dtype=int), j=0)
 direction_synapses.connect(i=np.arange(min_front,num_radar,dtype=int), j=1)
+
+# COMMAND MOTOR
 
 eqs_rad = '''
 vel_left : 1 (linked)
@@ -145,30 +153,19 @@ veldiff = vel_left - vel_right : 1 (constant over dt)
 radian = NeuronGroup(1, eqs_rad)
 radian.vel_left = linked_var(direction, 'vel', [0])
 radian.vel_right = linked_var(direction, 'vel', [1]) 
-   
-#s_yn1 = StateMonitor(ears, 'yn1', record=True)
-#s_yn2 = StateMonitor(ears, 'yn2', record=True)
-#s_xn1 = StateMonitor(ears, 'xn1', record=True)
-#s_xn2 = StateMonitor(ears, 'xn2', record=True)
-#s_xn = StateMonitor(ears, 'xn', record=True)
 
-radar_spikes = SpikeMonitor(radar)
-spikes = SpikeMonitor(neurons)
-combi = SpikeMonitor(combination)
+# MONITORS
+
 ears_spikes = SpikeMonitor(ears)
+neuron_spikes = SpikeMonitor(neurons)
+radar_spikes = SpikeMonitor(radar)
+combi_spikes = SpikeMonitor(combination)
 
-# Ne peut pas etre utilise en un seul state monitor
-#sound = StateMonitor(ears, 'yn', record=True)
-combi_state = StateMonitor(combination, 'v', record=True)
+ears_state = StateMonitor(ears, ['yn', 'yn1', 'yn2', 'xn', 'xn1', 'xn2', 'x', 'thresh'], record=True)
 radar_state = StateMonitor(radar, 'v', record=True)
-#thresh = StateMonitor(ears, 'thresh', record=True)
-#son = StateMonitor(ears, 'x', record=True)
-# dir = StateMonitor(direction, 'v', record=True)
-# dir_vel = StateMonitor(direction, 'vel', record=True)
+combi_state = StateMonitor(combination, 'v', record=True)
+dire_state = StateMonitor(direction, ['v', 'vel'], record=True)
 radian_state = StateMonitor(radian, 'veldiff', record=True)
-
-state_ears = StateMonitor(ears, ['yn', 'yn1', 'yn2', 'xn', 'xn1', 'xn2', 'x', 'thresh'], record=True)
-state_direction = StateMonitor(direction, ['v', 'vel'], record=True)
 
 run(10*second, report='text', report_period=1*second, profile=True)
 
